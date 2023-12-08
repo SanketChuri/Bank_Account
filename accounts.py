b6615b0ec0990640da74a7b6b21e13588bc2eb57
SanketChuri <sanketchuri456@gmail.com>
2023-12-08 02:30:40 +0000

    updated- only 3 errors found

--- accounts.py
+++ accounts.py
@@ -3,61 +3,118 @@ import datetime
 
 
 class BasicAccount:
-    account_counter = 0
+    account_number_counter = 1
 
-    def generate_account_number(self):
-        account_number = BasicAccount.account_counter
-        BasicAccount.account_counter += 1
-        return account_number
+    # def generate_account_number(self):
+    #     account_number = BasicAccount.account_number_counter
+    #     BasicAccount.account_number_counter += 1
+    #     return account_number
 
-    def generate_card_number(self):
-        return random.randint(1000000000000000, 9999999999999999)
+    # def generate_card_number(self):
+    #     return random.randint(1000000000000000, 9999999999999999)
 
-    def generate_card_expire(self):
-        current_year = datetime.date.today().year
-        month = random.randint(1, 12)
-        year = random.randint(current_year, current_year + 3)
-        return f"{month:02d}/{year % 100:02d}"
+    # def generate_card_expire(self):
+    #     current_year = datetime.date.today().year
+    #     month = datetime.date.today().month
+    #     year = current_year + 3
+    #     return f"({month}/{year % 100:02d})"
+    
+    def get_ac_num(self):
+        ac_num = BasicAccount.account_number_counter
+        BasicAccount.account_number_counter += 1
+        return ac_num
 
     def __init__(self, acc_name: str, opening_balance: float):
         self.name = acc_name
         self.ac_num = self.get_ac_num()
         self.balance = opening_balance
-        card_number = self.generate_card_number()
-        self.card_num = card_number
-        card_expire = self.generate_card_expire()
-        self.card_exp = card_expire
+
+    def __str__(self):
+        return f"Account Name: {self.name}, Account Number: {self.ac_num}, Balance: {self.balance}"
 
     def deposit(self, amount: float):
         self.balance += amount
         return amount
 
-    def get_balance(self):
-        return self.balance
-
     def withdraw(self, amount: float):
-        if amount <= self.balance:
-            self.balance -= amount
-            print(self.name, " has withdrawn £", amount, ".New balance is £", self.balance)
-            return
-        else:
-            print("Can not withdraw £", amount)
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"{self.name} has withdrawn £{amount}. New balance is £{self.balance}")
+                return 
+            else:
+                print(f"Can not withdraw £{amount}")
+                return 
+                
 
     def get_available_balance(self):
         available_balance = self.get_balance() - self.withdraw()
         return available_balance
 
+    def get_balance(self):
+        return self.balance
+    
+    def print_balance(self):
+        print(f"Balance: {self.balance}")
+
     def get_name(self):
         return self.name
+
     
-    def get_ac_num(self):
-        ac_num = BasicAccount.account_counter
-        BasicAccount.account_counter += 1
-        return ac_num
-    
+
     def issue_new_card(self):
-        current_year = datetime.date.today().year
-        month = random.randint(1, 12)
-        year = random.randint(current_year, current_year + 3)
-        card_exp = f"{month:02d}/{year % 100:02d}"
-        return card_exp
+
+        self.card_num = str(random.randint(1000000000000000, 9999999999999999))
+        expire_year = datetime.date.today().year + 3
+        self.card_exp = (datetime.date.today().month,expire_year % 100)
+        return self.card_num,self.card_exp
+    
+    def close_account(self):
+        self.balance = 0.0
+        return True
+    
+# account1 = BasicAccount( "John Doe", 1000)
+# account2 = BasicAccount( "Rose Ado", 1000)
+# account3 = BasicAccount( "Mary lolu", 1000)
+# print(account1.issue_new_card())
+# print(account2)
+# print(account3)
+    
+class PremiumAccount(BasicAccount):
+    def __init__(self, ac_name: str, opening_balance: float, initial_overdraft: float):
+        super().__init__(ac_name, opening_balance)
+        self.overdraft = False
+        self.overdraft_limit = initial_overdraft
+
+    def __str__(self):
+        return f"Account Name: {self.name}, Account Number: {self.ac_num}, Balance: {self.balance}, Overdraft: {self.overdraft}, Overdraft Limit: {self.overdraft_limit}"
+
+    def set_overdraft_limit(self, new_limit: float):
+        self.overdraft_limit = new_limit
+
+    def get_available_balance(self):
+        if self.balance >= 0:
+            return self.balance
+        elif self.balance < -self.overdraft_limit:
+            self.overdraft = True
+            return 0.0
+        else:
+            self.overdraft = True
+            return self.balance + self.overdraft_limit
+
+    def print_balance(self):
+        if self.overdraft:
+            print(f"Balance: {self.balance}, Available Balance: {self.get_available_balance()}, Overdraft: {self.overdraft}, Overdraft Limit: {self.overdraft_limit}")
+        else:
+            print(f"Balance: {self.balance}, Available Balance: {self.get_available_balance()}")
+
+    def close_account(self):
+        if self.get_available_balance() > 0:
+            print("Account cannot be closed. There is a balance.")
+            return False
+        else:
+            self.card_num = None
+            self.card_exp = None
+            self.balance = 0.0
+            self.overdraft = False
+            self.overdraft_limit = 0.0
+            return True
